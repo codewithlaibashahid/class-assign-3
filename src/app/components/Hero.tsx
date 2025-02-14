fc3904f531297cc1a1cd91ac175e9289fc607bf0
codewithlaibashahid <malikshazy32@gmail.com>
2025-02-15 01:14:10 +0500

    Hero.tsx

--- src/app/components/Hero.tsx
+++ src/app/components/Hero.tsx
@@ -3,7 +3,7 @@ import React from 'react'
 const Hero = () => {
   return (
    <main>
-    <h1 className='text-white text-center text-8xl pt-28 font-bold font-serif'>Hello! I am Tayyaba Shahid</h1>
+    <h1 className='text-white text-center text-8xl pt-28 font-bold font-serif'>Hello! I am Laiba Shahid</h1>
     <p className='text-white text-center text-3xl pt-10 '>
         "A passionate Web Developer ,Turning ideas into interactive web experiences with clean code and modern tech."</p>
    </main>
